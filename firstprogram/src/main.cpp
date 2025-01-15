42a7588321cec540d57bb96137ee52506b1fd454
st3rniel <martinoywa@icloud.com>
2025-01-15 14:11:51 +0300

    Add first program

---
+++ firstprogram/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int favorite_number;
+
+    cout << "Enter your favorite number between 1 and 100: ";
+    cin >> favorite_number;
+    cout << "Awesome! That's mine too!" << endl;
+
+    return 0;
+}
