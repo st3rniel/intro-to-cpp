0ee357a3f4106957ce0f912374eec4d45e038fc5
st3rniel <martinoywa@icloud.com>
2025-01-15 14:22:17 +0300

    Add section 4 challenge solution

---
+++ section4_challenge/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int favorite_number;
+
+    cout << "Enter your favorite number between 1 and 100: ";
+    cin >> favorite_number;
+
+    cout << "Awesome! That's mine too!" << endl;
+    cout << "No really!, " << favorite_number << " is my favorite number!" << endl;
+
+    return 0;
+}
