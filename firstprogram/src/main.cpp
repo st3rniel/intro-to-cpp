3534276ca2739f5adfc5072583ab603459e66aa4
st3rniel <martinoywa@icloud.com>
2025-01-16 03:16:45 +0300

    Add cout cin comments

--- firstprogram/src/main.cpp
+++ firstprogram/src/main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main() {
     int favorite_number;
 
-    cout << "Enter your favorite number between 1 and 100: ";
-    cin >> favorite_number;
+    cout << "Enter your favorite number between 1 and 100: ";   // << insertion operator
+    cin >> favorite_number;     // extraction operator
     cout << "Awesome! That's mine too!" << endl;
 
     return 0;
