02962d882971da1b58de2217f46d17ce8c0c912c
st3rniel <martinoywa@icloud.com>
2025-01-16 03:18:11 +0300

    Add section 5 challenge solutions

---
+++ section5_challenge/src/exercise_2.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Martin Oywa on 16/01/2025.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x {3};
+    cout << "Martin has " << x << " dogs.";
+    return 0;
+}
---
+++ section5_challenge/src/exercise_3.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Martin Oywa on 16/01/2025.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int m {};
+    int d {};
+    int y {};
+
+    cout << "What is your date of birth? (format: month day year) ";
+    cin >> m >> d >> y;
+
+    cout << m << "/" << d << "/" << y << endl;
+    return 0;
+}
