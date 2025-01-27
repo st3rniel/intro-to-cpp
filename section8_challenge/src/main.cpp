679bce4a69bdd47232bea7eabad7417531a445b8
st3rniel <martinoywa@icloud.com>
2025-01-27 13:29:44 +0300

    Add section 8 challenge solution

---
+++ section8_challenge/src/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> change(int denomination, int cents) {
+  vector<int> result;
+
+  result.push_back(cents/denomination); // add count
+  result.push_back(cents%denomination); // add reminder
+
+  return result;
+}
+
+int main() {
+
+	// cents per dollar denomination
+    int one_dollar {100};
+    int one_quarter {25};
+    int one_dime {10};
+    int one_nickel {5};
+    int one_penny {1};
+	vector<int> denominations {one_dollar, one_quarter, one_dime, one_nickel, one_penny};
+	vector<string> denomination_names {"Dollars", "Quarters", "Dimes", "Nickels", "Pennies"};
+
+	cout << "Change Calculator" << endl;
+	cout << "Enter an amount in cents: ";
+
+	int cents {0};
+	int cents_count {0};
+	int cents_reminder {0};
+
+	cin >> cents;
+	cents_reminder = cents;
+
+    cout << "You can provide this change as follows: " << endl;
+
+	for (int i = 0; i < denominations.size(); i++) {
+		vector<int> current_results = change(denominations.at(i), cents_reminder);
+		cents_count = current_results.at(0);
+		cents_reminder = current_results.at(1);
+		cout << denomination_names.at(i) << ": " << cents_count << endl;
+	}
+
+    return 0;
+}
