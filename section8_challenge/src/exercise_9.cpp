420d9fd3fde33f1f039496ec95d3d782bb884606
st3rniel <martinoywa@icloud.com>
2025-01-27 12:50:50 +0300

    Add section 8 exercise 8 solution

---
+++ section8_challenge/src/exercise_9.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Martin Oywa on 27/01/2025.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << boolalpha; // 0 is False and 1 is True
+
+    int age {0};
+    bool parental_consent {false};
+    bool ssn {false};
+    bool accidents {false};
+
+    cout << "Enter age: ";
+    cin >> age;
+    cout << "Enter parental consent (0/1): ";
+    cin >> parental_consent;
+    cout << "Enter Social Security Number (0/1): ";
+    cin >> ssn;
+    cout << "Enter accidents (0/1): ";
+    cin >> accidents;
+
+    bool eligible {false};
+    eligible = (age >= 18 || (age > 15 && parental_consent == true)) && ssn == true && accidents != true;
+
+    if (eligible) {
+        cout << "\nYes, you can work.";
+    } else {
+        cout << "\nNo, you can't work.";
+    }
+
+    return 0;
+}
