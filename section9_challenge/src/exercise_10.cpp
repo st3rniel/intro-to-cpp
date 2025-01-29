5884698262c30641d3218f2e2ed49548eceddadb
st3rniel <martinoywa@icloud.com>
2025-01-29 21:54:01 +0300

    Update section 9 exercise 10 solution

---
+++ section9_challenge/src/exercise_10.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Martin Oywa on 29/01/2025.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int age {0};
+    cin >> age;
+
+    if (age >= 16) {
+        cout << "Yes - you can drive!";
+    }
+
+    return 0;
+}
