ee0f88b68ecd1aa52f3a497da232f9291ee8f169
st3rniel <martinoywa@icloud.com>
2025-07-07 09:49:35 +0300

    Update MIT6_096IAP11_assn02 setup code folder

---
+++ MIT6_096IAP11_assn02/src/TestFunctions.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Martin Oywa on 07/07/2025.
+//
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
